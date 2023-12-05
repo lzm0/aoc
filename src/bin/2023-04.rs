15996f7fd25aaa2f9ffe085c1ce4199b89717d74
Zimo Li <7163127+lzm0@users.noreply.github.com>
2023-12-04 21:05:36 -0500

    Use `&str` for test input

--- src/bin/2023-04.rs
+++ src/bin/2023-04.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn parse_input(input: &String) -> Vec<(Vec<u8>, Vec<u8>)> {
+fn parse_input(input: &str) -> Vec<(Vec<u8>, Vec<u8>)> {
     input
         .lines()
         .map(|line| {
@@ -72,8 +72,7 @@ Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
-        .to_string();
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
     let cards = parse_input(&input);
     assert_eq!(part_one(&cards), 13);
 }
@@ -85,8 +84,7 @@ Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
-        .to_string();
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
     let cards = parse_input(&input);
     assert_eq!(part_two(&cards), 30);
 }
